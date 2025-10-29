e83c367112f6b956e92c25520ec2589e02acacb3
AfnanHaque12 <abirafnan204@gmail.com>
2025-10-29 14:21:26 +0600

    23-55098-3

---
+++ TASK1/TASK1/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Enter 3 number :");
+int student1 = Convert.ToInt32(Console.ReadLine());
+int student2 = Convert.ToInt32(Console.ReadLine());
+int student3 = Convert.ToInt32(Console.ReadLine());
+
+double average = (student1 + student2 + student3) / 3;
+Console.WriteLine($"Average score :{average}");
