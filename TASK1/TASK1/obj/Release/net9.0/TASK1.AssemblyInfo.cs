ed7e27e1db834d54f5e1404950d0473ac47f310f
AfnanHaque12 <abirafnan204@gmail.com>
2025-11-03 15:05:39 +0600

    23-55098-3

--- TASK1/TASK1/obj/Release/net9.0/TASK1.AssemblyInfo.cs
+++ TASK1/TASK1/obj/Release/net9.0/TASK1.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("TASK1")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a694e437ceac60b1f3b8ff3b726603168aa87a98")]
 [assembly: System.Reflection.AssemblyProductAttribute("TASK1")]
