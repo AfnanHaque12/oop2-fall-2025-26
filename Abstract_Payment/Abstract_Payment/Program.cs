1ed9102bef71976a3ceda18e3999bb773addddad
AfnanHaque12 <abirafnan204@gmail.com>
2025-12-14 21:56:35 +0600

    23-55098-3

---
+++ Abstract_Payment/Abstract_Payment/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+abstract class Payment
+{
+ public abstract void ProcessPayment(double amount);
+}
+class BkashPayment : Payment
+{
+public override void ProcessPayment(double amount)
+{
+  Console.WriteLine("Processing Bkash payment: " + amount);
+}
+}
+class CreditCardPayment : Payment
+{
+public override void ProcessPayment(double amount)
+{
+   Console.WriteLine("Processing Credit Card payment: " + amount);
+}
+}
+class Program
+{
+static void Main()
+{
+     Payment p1 = new BkashPayment();
+     Payment p2 = new CreditCardPayment();
+
+     Console.WriteLine("Bkash Payment:");
+     p1.ProcessPayment(1000);
+
+     Console.WriteLine("Credit Card Payment:");
+     p2.ProcessPayment(5000);
+     Console.ReadLine();
+}
+}
