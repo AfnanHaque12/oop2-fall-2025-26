056117a2a34f555fa6b64b61927afadcc4e26d51
AfnanHaque12 <abirafnan204@gmail.com>
2025-12-14 21:37:06 +0600

    23-55098-3

---
+++ Abstract_Member/Abstract_Member/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+abstract class Member
+{
+    private string name;
+    private int memberId;
+ public Member(string name, int memberId)
+  {
+     this.name = name;
+     this.memberId = memberId;
+  }
+   public abstract double CalculateFee();
+}
+class StudentMember : Member
+{
+ public StudentMember(string name, int id) : base(name, id) { }
+ public override double CalculateFee()
+ {
+    return 300;
+ }
+}
+class RegularMember : Member
+{
+ public RegularMember(string name, int id) : base(name, id) { }
+public override double CalculateFee()
+{
+   return 500;
+ }
+}
+class Program
+{
+static void Main()
+{
+   Member s1 = new StudentMember("A",100);
+   Member r1 = new RegularMember("B",101);
+
+   Console.WriteLine($"Student Member Fee: " + s1.CalculateFee());
+   Console.WriteLine($"Regular Member Fee: " + r1.CalculateFee());
+}
+}
+
